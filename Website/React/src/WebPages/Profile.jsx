6a63b33a900f540af1d2d36d9dc89955bcb2c9dd
saitharun-09 <saitharun.kadam@gmail.com>
2025-09-21 15:45:06 +0530

    Added Profile Page

---
+++ Website/React/src/WebPages/Profile.jsx
@@ -0,0 +1,21 @@
+import Header from "./Header";
+
+function Profile() {
+    return (
+        <>                  
+            <Header />
+
+            <div className="profileInfo">
+                <P>First Name : </P>
+                <p>Last Name :</p>
+                <p>Email :</p>
+                <p>Date of Birth : </p>
+                <p>Address : </p>
+                <button>Change Password</button>
+                <button>Logout</button>
+            </div>
+        </>
+    );
+}
+
+export default Profile;
